2143ae4c09fd7eebde05e33a8192395fa5077754
Václav Blažek <vaclav.blazek@citationtech.net>
2012-09-27 22:58:22 +0000

    geometry/kdtree: not-damaging associated vector, moved nodes from tree itself
    git-svn-id: https://scm.citationtech.net/trunk/shared/geometry/geometry@2090 b7e6147c-0f76-4c84-a09d-19069afaf10c

--- geometry/kdtree.hpp
+++ geometry/kdtree.hpp
@@ -1,8 +1,10 @@
 /**
  *  @file kdtree.hpp
  *  @author Jakub Cerveny <jakub.cerveny@ext.citationtech.net>
+ *  @author Vaclav Blazek <vaclav.blazek@ext.citationtech.net>
  *
  *  The k-d tree data structure for fast nearest neighbor search.
+ *  2011-09-28    refactored
  */
 
 #ifndef KDTREE_HPP_INCLUDED
@@ -24,46 +26,49 @@ namespace geometry {
  *
  *  The type T is a point in space and K is the number of dimensions.
  *  The coordinates of T must be accessible with operator().
+ *
+ *  NB: Associated vector must outlive this tree!
+ *  Associated vector is not modified by any means.
  */
 
-template<typename T, int K = 3>
-class KdTree : boost::noncopyable
+namespace detail {
+
+template<typename T, unsigned int K = 3>
+class KdTreeNode : boost::noncopyable
 {
 public:
+    typedef typename std::vector<T>::const_iterator iterator;
 
-    typedef typename std::vector<T>::iterator iterator;
-    typedef typename std::vector<T>::const_iterator const_iterator;
+    typedef std::vector<iterator> Indirect;
 
-    /**
-     *  Builds the tree from a vector of points. You need to pass the beginning
-     *  and end iterators of the vector. The vector is not read-only -- it will
-     *  be modified (sorted) by this constructor. "depth" must be zero.
-     */
-    KdTree(iterator beg, iterator end, int depth = 0)
+    KdTreeNode(typename Indirect::iterator beg, typename Indirect::iterator end
+               , int depth = 0)
         : sons({ nullptr, nullptr })
     {
         // trivial case - just one point
-        if (beg+1 >= end)
-        {
-            point = beg;
+        if (beg + 1 >= end) {
+            point = *beg;
             return;
         }
 
         // sort points by one of the coordinates
-        int axis = depth % K;
-        std::sort(beg, end, [axis](const T& a, const T& b)
-                                  { return a(axis) < b(axis); } );
+        unsigned int axis = depth % K;
+        std::sort(beg, end, [axis](const iterator& a, const iterator& b)
+                  { return (*a)(axis) < (*b)(axis); } );
 
         // the median will be the content (and boundary) of this node
-        int count = end - beg;
-        iterator median = beg + count/2;
-        point = median;
+        auto count(std::distance(beg, end));
+        auto median(beg + count / 2);
+        point = *median;
 
         // create two subtrees (points smaller and larger than the median)
-        if (median > beg) sons[0] = new KdTree(beg, median, depth+1);
-        if (median+1 < end) sons[1] = new KdTree(median+1, end, depth+1);
+        if (median > beg) {
+            sons[0] = new KdTreeNode(beg, median, depth + 1);
+        }
+        if (median+1 < end) {
+            sons[1] = new KdTreeNode(median + 1, end, depth + 1);
+        }
     }
-
 
     /**
      *  Finds the nearest neighbor of the point "query" (this is a general point
@@ -71,37 +76,40 @@ public:
      *  the tree and "dist2" is filled with the square of the distance.
      *  "axis" must be zero.
      */
-    template<int IgnoreEqual = false>
-    iterator nearest(const T& query, double& dist2, int axis = 0) const
+    template<bool IgnoreEqual = false>
+    iterator nearest(const T& query, double& dist2, unsigned int axis = 0)
+        const
     {
         // calculate the squared distance between query and us
         T diff(query - *point);
         dist2 = inner_prod(diff, diff);
 
         // if a point already in the tree is queried we may want to ignore it
-        if (IgnoreEqual && dist2 == 0.0)
+        if (IgnoreEqual && dist2 == 0.0) {
             dist2 = INFINITY;
+        }
 
         // if this is a leaf, we're done
         iterator result = point;
-        if (!sons[0] && !sons[1])
+        if (!sons[0] && !sons[1]) {
             return result;
+        }
 
         // perpendicular distance to node boundary
         double perp = query(axis) - (*point)(axis);
 
         // change axis
-        if (++axis >= K) axis = 0;
+        if (++axis >= K) {
+            axis = 0;
+        }
 
         double sub2;
 
         // search the near side of the tree first
-        int side = (perp < 0) ? 0 : 1;
-        if (sons[side])
-        {
+        unsigned int side = (perp < 0) ? 0 : 1;
+        if (sons[side]) {
             iterator it = sons[side]->nearest<IgnoreEqual>(query, sub2, axis);
-            if (sub2 < dist2)
-            {
+            if (sub2 < dist2) {
                 result = it;
                 dist2 = sub2;
 
@@ -114,11 +122,9 @@ public:
 
         // we have to search the other side too
         side ^= 1;
-        if (sons[side])
-        {
+        if (sons[side]) {
             iterator it = sons[side]->nearest<IgnoreEqual>(query, sub2, axis);
-            if (sub2 < dist2)
-            {
+            if (sub2 < dist2) {
                 result = it;
                 dist2 = sub2;
             }
@@ -127,20 +133,12 @@ public:
         return result;
     }
 
-    template<int IgnoreEqual = false>
-    std::pair<iterator, double> nearest(const T& query) const
-    {
-        std::pair<iterator, double> res;
-        res.first = nearest<IgnoreEqual>(query, res.second);
-        return res;
-    }
-
     /**
      *  Returns all points that are within "radius" from "query".
      */
-    template<int IgnoreEqual = false>
+    template<bool IgnoreEqual = false>
     void range(const T& query, double radius,
-               std::vector<T>& result, int axis = 0) const
+               std::vector<T>& result, unsigned int axis = 0) const
     {
         // return point if it is within radius
         T diff(query - *point);
@@ -154,7 +152,9 @@ public:
         double perp = query(axis) - (*point)(axis);
 
         // change axis
-        if (++axis >= K) axis = 0;
+        if (++axis >= K) {
+            axis = 0;
+        }
 
         // recurse to sub-trees if they are within radius
         if (sons[0] && perp <= +radius) {
@@ -165,33 +165,83 @@ public:
         }
     }
 
-    ~KdTree()
-    {
+    ~KdTreeNode() {
         delete sons[0];
         delete sons[1];
     }
 
 protected:
-
-    static double inner_prod( const T & op1, const T & op2 ) {
-        
+    static double inner_prod(const T &op1, const T &op2) {
         double retval( 0.0 );
-        for ( int i = 0; i < K; i++ )
+        for (unsigned int i = 0; i < K; i++) {
             retval += op1(i) * op2(i);
+        }
         return retval;
     };
-    
+
 
     iterator point;
-    KdTree* sons[2];
+    KdTreeNode *sons[2];
 
     /* TODO: since the tree is perfectly balanced its structure is known
        implicitly and we could operate just on the point array with no
        additional nodes and pointers */
 };
 
+} // namespace detail
+
+template<typename T, unsigned int K = 3>
+class KdTree : boost::noncopyable
+{
+public:
+    typedef typename std::vector<T>::const_iterator iterator;
+
+    KdTree(iterator beg, iterator end)
+        : root_(nullptr)
+    {
+        typename detail::KdTreeNode<T, K>::Indirect indirect;
+        indirect.reserve(std::distance(beg, end));
+        for ( ; beg != end; ++beg) {
+            indirect.push_back(beg);
+        }
+
+        root_ = new detail::KdTreeNode<T, K>(indirect.begin(), indirect.end());
+    }
+
+    template<bool IgnoreEqual = false>
+    iterator nearest(const T& query, double& dist2) const
+    {
+        return root_->nearest(query, dist2);
+    }
+
+    template<bool IgnoreEqual = false>
+    std::vector<T> range(const T& query, double radius) const {
+        std::vector<T> result;
+        root_->range<IgnoreEqual>(query, radius, result);
+        return result;
+    }
+
+    template<bool IgnoreEqual = false>
+    std::pair<iterator, double> nearest(const T& query) const
+    {
+        std::pair<iterator, double> res;
+        res.first = root_->nearest<IgnoreEqual>(query, res.second);
+        return res;
+    }
+
+    template<bool IgnoreEqual = false>
+    void range(const T& query, double radius, std::vector<T>& result) const {
+        return root_->range<IgnoreEqual>(query, radius, result);
+    }
+
+    ~KdTree() {
+        delete root_;
+    }
+
+private:
+    detail::KdTreeNode<T, K> *root_;
+};
 
 } // namespace geometry
 
 #endif
-
