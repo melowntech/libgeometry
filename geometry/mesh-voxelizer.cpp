02893579b327ade07ab8219c11ccaffebdde79aa
Vaclav Blazek <vaclav.blazek@citationtech.net>
2018-04-23 18:27:29 +0200

    using utility/openmp

--- geometry/mesh-voxelizer.cpp
+++ geometry/mesh-voxelizer.cpp
@@ -23,12 +23,8 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#ifdef _OPENMP
-# include <omp.h>
-#else
-# define omp_get_max_threads() 1
-# define omp_get_thread_num() 0
-#endif
+
+#include "utility/openmp.hpp"
 
 #include "geometry/meshop.hpp"
 #include "geometry/mesh-voxelizer.hpp"
