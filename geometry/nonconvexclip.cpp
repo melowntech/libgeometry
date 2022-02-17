c4256dc16970a77e8ed59136186bd78769d1d3ee
Tomáš Pazdiora <tomas.pazdiora@melowntech.com>
2022-02-17 17:40:52 +0100

    hotfix for clipping vertical triangles

--- geometry/nonconvexclip.cpp
+++ geometry/nonconvexclip.cpp
@@ -100,8 +100,25 @@ math::Triangles3d clipTriangleNonconvex(const math::Triangle3d &tri_,
     bool flip = false;
     double ccw(checkCcw(tri2[0], tri2[1], tri2[2]));
 
-    if (std::abs(ccw) < 1e-4) {
-        return {}; // TODO: handle exactly vertical triangles
+    // convert clipRegion to boost MultiPolygon
+    MultiPolygon clipMultiPoly;
+    for (const auto &pts : clipRegion) {
+        Polygon part;
+        bg::assign_points(part, bgPoints(pts));
+        clipMultiPoly.push_back(part);
+    }
+
+    if (std::abs(ccw) < 1e-4) 
+    {
+        // TODO: handle exactly vertical triangles properly
+        if (bg::within(Point{tri[0](0), tri[0](1)}, clipMultiPoly) && 
+            bg::within(Point{tri[1](0), tri[1](1)}, clipMultiPoly) && 
+            bg::within(Point{tri[2](0), tri[2](1)}, clipMultiPoly)) 
+        {
+            return {tri};
+        } else {
+            return {};
+        }
     }
 
     // ensure counter-clockwise orientation for clipping
@@ -112,19 +129,12 @@ math::Triangles3d clipTriangleNonconvex(const math::Triangle3d &tri_,
     }
 
     // convert input to 2D polygons
-    Polygon poly1;
-    bg::assign_points(poly1, bgPoints(tri));
-
-    MultiPolygon poly2;
-    for (const auto &pts : clipRegion) {
-        Polygon part;
-        bg::assign_points(part, bgPoints(pts));
-        poly2.push_back(part);
-    }
+    Polygon trianglePoly;
+    bg::assign_points(trianglePoly, bgPoints(tri));
 
     // calculate intersection
     std::deque<Polygon> isect;
-    bg::intersection(poly1, poly2, isect);
+    bg::intersection(trianglePoly, clipMultiPoly, isect);
 
     // triangulate
     math::MultiPolygon isect2;
